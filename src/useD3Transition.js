131e6ae3ec0bee47ea296b399f2f1e2997a655ef
davidnmora <davidnmora@gmail.com>
2020-04-12 15:55:04 -0700

    Cleanup code with prettier && better naming (remove unneeded object reducer)

--- src/useD3Transition.js
+++ src/useD3Transition.js
@@ -1,63 +1,55 @@
-import {useRef, useEffect, useState} from 'react'
-import {select as d3Select} from 'd3-selection'
-import 'd3-transition';
+import { useRef, useEffect, useState } from 'react'
+import { select as d3Select } from 'd3-selection'
+import 'd3-transition'
 
 const DEFAULT_TRANSITION_DURATION = 800
 
+const useD3Transition = ({
+  attrsToTransitionTo,
+  deps,
 
-const useD3AttrTransition = ({
-															 attrsToTransitionTo,
-															 deps,
-	
-															 attrsToTransitionFromInitially = null,
-															 duration = null,
-															 easingFunction = null,
-														 }) => {
-	
-	const ref = useRef(null)
-	const [attrState, setAttrState] = useState(attrsToTransitionFromInitially || attrsToTransitionTo)
-	
-	const callback = () => {
-		if (!ref.current) return
-		
-		const element = d3Select(ref.current);
-		const transition = element
-			.transition()
-			.duration(typeof(duration) === 'number' ? duration : DEFAULT_TRANSITION_DURATION)
-		
-		if (easingFunction) {
-			transition.ease(easingFunction)
-		}
-		
-		const attrNames = Object.keys(attrState)
-		
-		attrNames.forEach(attrName => {
-			transition.attr(attrName, attrsToTransitionTo[attrName])
-		})
-		
-		transition.on('end', () => {
-			if (!ref.current) return
-			setAttrState(
-				attrNames.reduce(
-					(accumulator, attrName) => (
-						{
-							...accumulator,
-							[attrName]: attrsToTransitionTo[attrName],
-						}
-					),
-					{}
-				)
-			)
-		})
-		return () => element.interrupt() // cleanup by ending transitions
-	}
-	
-	useEffect(callback, deps)
-	
-	return {
-		ref,
-		attrState,
-	}
+  attrsToTransitionFromInitially = null,
+  duration = null,
+  easingFunction = null,
+}) => {
+  const ref = useRef(null)
+  const [attrState, setAttrState] = useState(
+    attrsToTransitionFromInitially || attrsToTransitionTo
+  )
+
+  const executeD3Transition = () => {
+    if (!ref.current) return
+
+    const element = d3Select(ref.current)
+    const transition = element
+      .transition()
+      .duration(
+        typeof duration === 'number' ? duration : DEFAULT_TRANSITION_DURATION
+      )
+
+    if (easingFunction) {
+      transition.ease(easingFunction)
+    }
+
+    const attrNames = Object.keys(attrState)
+
+    attrNames.forEach((attrName) => {
+      transition.attr(attrName, attrsToTransitionTo[attrName])
+    })
+
+    transition.on('end', () => {
+      if (!ref.current) return
+      setAttrState(attrNames)
+    })
+    return () => element.interrupt() // cleanup by ending transitions
+  }
+
+  useEffect(executeD3Transition, deps)
+
+  return {
+    ref,
+    attrState,
+  }
 }
 
-export default useD3AttrTransition
+export default useD3Transition
